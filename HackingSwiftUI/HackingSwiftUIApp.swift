77a1c764a77f6fb1ac0e6d7d860fe7b8cce4bf0e
Haktan Can Taşkıran <haktancantaskiran@gmail.com>
2024-03-14 21:16:56 +0300

    Initial Commit

---
+++ HackingSwiftUI/HackingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingSwiftUIApp.swift
+//  HackingSwiftUI
+//
+//  Created by Haktan Can Taşkıran on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct HackingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
